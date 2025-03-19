28f766005e2e62e1f0fb67e7032941b757cfbab0
Dev Uttam Bharti <114516227+devbharti466@users.noreply.github.com>
2025-03-19 23:23:43 +0530

    Update App.py

--- App.py
+++ App.py
@@ -2,17 +2,14 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# ✅ Load Merged Dataset
 df = pd.read_csv("data\\flixpatrol.csv")
 
-# Ensure Premiere is numeric
 df["Premiere"] = pd.to_numeric(df["Premiere"], errors='coerce')
 
 df["Watchtime in Million"] = df["Watchtime in Million"].str.replace("M", "").astype(float)  # Convert watchtime to numeric
 
 st.title("Netflix Movie Recommendation System")
 
-# User Inputs
 movie_type = st.selectbox("Select Type:", ["Movie", "TV Show"])
 genre = st.selectbox("Select Genre:", df["Genre"].unique())
 year_range = st.slider("Select Year Range:", 2000, 2023, (2010, 2020))
@@ -23,20 +20,16 @@ analysis_option = st.selectbox("Select Analysis Type:", [
     "Customer Complaints & Suggestions"
 ])
 
-# Filter dataset
 filtered_data = df[(df["Type"] == movie_type) & (df["Genre"].str.contains(genre, na=False)) & (df["Premiere"].between(year_range[0], year_range[1], inclusive="both"))]
 
-# Sort based on selected viewing preference
 if view_option == "Most Viewed":
     filtered_data = filtered_data.sort_values(by="Watchtime in Million", ascending=False)
 else:
     filtered_data = filtered_data.sort_values(by="Watchtime in Million", ascending=True)
 
-# ✅ Display Filtered Data in Proper Order
 st.subheader("Filtered Data Table (Sorted by Watchtime)")
 st.write(filtered_data[["Title", "Genre", "Premiere", "Watchtime in Million"]].reset_index(drop=True).head(10))
 
-# Perform Analysis Based on User Selection
 if analysis_option == "Netflix Insights":
     st.subheader("Insights for Netflix")
     netflix_problems = [
